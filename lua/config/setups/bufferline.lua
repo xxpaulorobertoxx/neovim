95e5612d037991ea21dc8b813db5a6fc2d02d201
Paulo Roberto Silva <paulo.pinda@gmail.com>
2026-02-03 22:23:17 -0300

    fixes

--- lua/config/setups/bufferline.lua
+++ lua/config/setups/bufferline.lua
@@ -44,7 +44,7 @@ function M.setup()
         }
       },
       color_icons = false,
-      show_buffer_icons = true,
+      show_buffer_icons = false,
       show_buffer_close_icons = true,
       show_close_icon = true,
       show_tab_indicators = true,
